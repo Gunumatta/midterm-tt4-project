c2323de6121fa97f4bc976db12ad5d8beb14d3e0
gurleenmatta <gurleen.m@somaiya.edu>
2025-02-25 16:14:29 -0500

    Finalizing project with updates and new files

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -7,8 +7,7 @@ module.exports = {
   entry: {
     global: path.resolve(__dirname, "global.js"),
     index: path.resolve(__dirname, "index.js"),
-    // listProducts: "./list-products.js",
-    // addProducts: "./add-products.js",
+    listProducts: path.resolve(__dirname, "list-products.js"),  // Ensure this file exists
   },
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -20,75 +19,75 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", {
-          loader: "sass-loader",
-          options: {
-            sassOptions: {
-              quietDeps: true,
-              includePaths: [path.resolve(__dirname, "node_modules")],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sassOptions: {
+                quietDeps: true,
+                includePaths: [path.resolve(__dirname, "node_modules")],
+              },
             },
-          },
-        },],
+          }
+        ],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: "babel-loader"
       },
       {
-        test: /\.(png|jpg|gif)$/,
-        type: "asset/resource", // Para copiar arquivos de imagem
+        test: /\.(png|jpg|gif|svg)$/,
+        type: "asset/resource",
         generator: {
-          filename: 'assets/images/[name][hash][ext][query]', // Colocando as imagens dentro de assets/images/
-        },
+          filename: 'assets/images/[name][hash][ext][query]'
+        }
       },
       {
-        test: /\.(woff(2)?|eot|ttf)$/,
-        type: "asset/resource", // Para copiar arquivos de fontes
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
         generator: {
-          filename: 'assets/fonts/[name][hash][ext][query]', // Colocando as fontes dentro de assets/fonts/
-        },
-      },      
-    ],
+          filename: 'assets/fonts/[name][hash][ext][query]'
+        }
+      }
+    ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name].css",
+      filename: "[name].css"
     }),
     new HtmlWebpackPlugin({
       template: "./index.html",
       chunks: ["global", "index"],
-      filename: "index.html",
+      filename: "index.html"
     }),
-    // new HtmlWebpackPlugin({
-    //   template: "./list-products.html",
-    //   chunks: ["listProducts", "global"],
-    //   filename: "list-products.html",
-    // }),
-    // new HtmlWebpackPlugin({
-    //   template: "./add-products.html",
-    //   chunks: ["addProducts", "global"],
-    //   filename: "add-products.html",
-    // }),
+    new HtmlWebpackPlugin({
+      template: "./list-products.html",
+      chunks: ["listProducts", "global"],
+      filename: "list-products.html"
+    }),
+    // Removed add-products HtmlWebpackPlugin instance
   ],
   optimization: {
     minimize: true,
     minimizer: [
-      `...`,
-      new CssMinimizerPlugin(),
-    ],
+      '...', // Use existing CSS and JS minimizers
+      new CssMinimizerPlugin()
+    ]
   },
   devServer: {
-    static: "./dist",
+    static: path.resolve(__dirname, "dist"),
     port: 9000,
     open: true,
-    hot: true,
+    hot: true
   },
   resolve: {
     alias: {
-      assets: path.resolve(__dirname, "assets"),
+      assets: path.resolve(__dirname, "assets")
     },
-    extensions: [".js", ".scss"],
+    extensions: ['.js', '.jsx', '.scss']
   },
-  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  mode: process.env.NODE_ENV === "production" ? "production" : "development"
 };
